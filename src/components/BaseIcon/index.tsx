a69c674365fbcbbb27d042b195a2f96150ee1cff
Victor Santiago <victorhsantiago3@gmail.com>
2025-01-23 22:54:32 +0100

    feat: add BaseIcon component

---
+++ src/components/BaseIcon/index.tsx
@@ -0,0 +1,33 @@
+interface BaseIconProps {
+  icon: string;
+  iconHexColor?: string;
+  size?: number;
+}
+
+/**
+ * BaseIcon component
+ * @param icon icon names avaible at https://fonts.google.com/icons?icon.size=24&icon.color=%2380A4AE&icon.style=Rounded
+ * @param iconHexColor hex color
+ * @param size rem units
+ * @returns
+ */
+function BaseIcon({
+  icon,
+  iconHexColor = '#ffffff',
+  size = 1.5,
+}: BaseIconProps) {
+  return (
+    <span
+      className="material-symbols-rounded"
+      style={{
+        color: iconHexColor,
+        fontSize: `${size}rem`,
+      }}
+      aria-hidden="true"
+    >
+      {icon}
+    </span>
+  );
+}
+
+export default BaseIcon;
